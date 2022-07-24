d02b32ff3505bed38373a126090b146db5e60182
Samarpan Harit <samarpan.harit@unicommerce.com>
2022-07-24 16:23:23 +0530

    Added main markup

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,31 @@
-import { useState } from 'react'
-import './App.css'
-import Stepper from './components/Stepper'
+import { useContext, useState } from "react";
+import Logo from "./components/Logo";
+import Stepper from "./components/Stepper";
+import StepBody from "./components/StepBody";
+import { StepsContext } from "./context/stepsContext";
+import stepsData from "@src/data/steps";
 
 function App() {
-
-  return (
-    <div>
-      Hello!
-    </div>
-  )
+	const [step] = useContext(StepsContext);
+	const stepData = stepsData[step];
+	const StepHeader = stepData.header;
+	return (
+		<div className="flex flex-col items-center justify-center h-screen px-3">
+			<div className="flex flex-col items-center justify-center w-full h-full">
+				<header className="flex items-center justify-center w-full">
+					<Logo />
+					<span className="text-3xl font-bold">Eden</span>
+				</header>
+				<main className="flex flex-col items-center justify-center w-full">
+					<div className="flex flex-col items-center w-full">
+						<Stepper />
+						<StepHeader title={stepData.title} subtitle={stepData.subtitle} />
+					</div>
+					<StepBody />
+				</main>
+			</div>
+		</div>
+	);
 }
 
-export default App
+export default App;
