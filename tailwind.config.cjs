11f1ef5836263060bf2759486e8cc395b3f919e0
Samarpan Harit <samarpan.harit@unicommerce.com>
2022-07-24 16:13:59 +0530

    Added tailwind and vite config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,15 +1,18 @@
 /** @type {import('tailwindcss').Config} */
-const colors = require('tailwindcss/colors');
+const defaultTheme = require("tailwindcss/defaultTheme");
+const colors = require("tailwindcss/colors");
 module.exports = {
-  content: ["./src/**/*.{jsx,tsx}"],
-  theme: {
-    extend: {},
-    colors: {
-      ...colors,
-      primary: {
-        DEFAULT: '#644CE5',
-      },
-    },
-  },
-  plugins: [],
-}
+	content: ["./src/**/*.{jsx,tsx}"],
+	theme: {
+		extend: {},
+		colors: {
+			...colors,
+			current: "currentColor",
+			primary: {
+				DEFAULT: "#644CE5",
+			},
+		},
+		fontFamily: { sans: ["Inter", ...defaultTheme.fontFamily.sans] },
+	},
+	plugins: [],
+};
